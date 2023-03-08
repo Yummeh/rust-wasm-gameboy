589ebe1c4b9376b57e3c3c8f2713adbab7b04d50
Ralph de Horde <ralph.de.horde@plat4mation.com>
2023-03-08 17:14:51 +0100

    Some ram stuff

--- gameboy-emu/src/internals/cpu.rs
+++ gameboy-emu/src/internals/cpu.rs
@@ -2,6 +2,13 @@ pub fn wow() {
     print!(" WWOOWW ");
 }
 
+const GAMEBOY_W: u8 = 160;
+const GAMEBOY_H: u8 = 144;
+
 struct CPU {
-    ram: [u32; 1000],
+    wram: [u16; 16384], // GBC 32 KiB = 32768 bytes = 16384 u16 (2 * u8)
+    vram: [u16; 8192], // GBC 16 KiB
+
+
+
 }
