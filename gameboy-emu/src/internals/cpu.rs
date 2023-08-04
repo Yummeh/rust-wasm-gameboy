ea21b04b223155d5f5953c9f625158b7b1f7e9e9
Ralph de Horde <ralphdehorde@gmail.com>
2023-08-05 00:03:35 +0200

    Updated package json

--- gameboy-emu/src/internals/cpu.rs
+++ gameboy-emu/src/internals/cpu.rs
@@ -5,10 +5,35 @@ pub fn wow() {
 const GAMEBOY_W: u8 = 160;
 const GAMEBOY_H: u8 = 144;
 
+trait GBDisplayable {
+    fn display(&mut self);
+}
+
+trait GBInput {
+    fn get_input();
+}
+
+trait GBAudio {
+    fn play_audio();
+}
+
 struct CPU {
     wram: [u16; 16384], // GBC 32 KiB = 32768 bytes = 16384 u16 (2 * u8)
-    vram: [u16; 8192], // GBC 16 KiB
+    vram: [u16; 8192],  // GBC 16 KiB
+}
+
+impl CPU {
+    pub fn cycle<T, S, U>(&self, renderer: &mut T, input: &mut S, audio: &mut U)
+    where
+        T: GBDisplayable,
+        S: GBInput,
+        U: GBAudio,
+    {
+        // read opcode from memory
+        // increase pc
+        // call function
 
 
-
+        // renderer.display(self.vram);
+    }
 }
